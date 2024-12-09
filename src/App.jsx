d8ed496274421e83470fc87d94d1cf4e0328d9f7
Asta1984 <salilmandal908@gmail.com>
2024-12-09 13:39:11 +0530

    ui revamped

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { Navbar } from './components/navbar';
 import { Hero } from './components/hero';
-import PointerEffect from './components/Pointereffect';
+
 
 function App() {
   return (
     <>
-      <PointerEffect />
-      <>
       <Navbar />
       <Hero />
-      </>
     </>
   );
 }
